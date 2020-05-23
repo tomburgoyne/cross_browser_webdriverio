4858ee0d43f59a46ab7f9ab2dc24a68fa22ac226
tomburgoyne <tomburgoyne@live.co.uk>
2020-05-23 20:22:19 +0100

    Added example of using JSON data file to generate tests dynamically

--- test/specs/example.spec.js
+++ test/specs/example.spec.js
@@ -19,3 +19,20 @@ describe("Example page - WebdriverIO Getting Started page", () => {
         await pageHeaderTitle.waitForExist();
     });
 });
+
+describe("Example page - using a JSON data file to generate tests", () => {
+    // create a new object of just the elements object returned from the .json file using require to get the file
+    const elements = require("../data/example.data.json").elements;
+    // use array.map to iterate/loop through elements object and create a test per element object found
+    elements.map((element) => {
+        it(`Checks that ${element.description} exists on page`, async () => {
+            const el = await $(element.locator);
+            await el.waitForExist();
+        });
+        // you would not normally have taking a screenshot as a 'test' - this is just an example of doing it
+        it(`Takes a screenshot of ${element.description}`, async () => {
+            const el = await $(element.locator);
+            await el.saveScreenshot(`./results/${element.description}.png`);
+        });
+    });
+});
